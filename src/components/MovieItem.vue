b6b214bee15f9d8268105760075250dbec050a77
JulieJulie <jtatica2002@gmail.com>
2023-07-20 10:24:30 -0500

    production tested

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -7,13 +7,20 @@ defineProps({
   }
 })
 
+const getImgURL = (imageName)=>{
+  const imgUrl = new URL(`/src/assets/images/${imageName}`, import.meta.url).href
+  console.log(imgUrl)
+  return imgUrl
+
+}
+
 const count = ref(0)
 </script>
 
 <template>
   
     <div class="bg-gray-300 dark:bg-slate-700 p-4 rounded-lg h-full" >
-        <img :src="'src/assets/'+movie.image" :alt=" movie.name" class="w-full h-[85%] rounded-lg shadow-lg">
+        <img :src="getImgURL(movie.image)" :alt=" movie.name" class="w-full h-[85%] rounded-lg shadow-lg">
         <p>{{movie.name }}</p>
     </div>
   
--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -11,12 +11,19 @@ const route = useRoute()
 const id=route.params.id
 
 const movie=movieData.find((movie) => movie.id == id);
+
+const getImgURL = (imageName)=>{
+  const imgUrl = new URL(`/src/assets/images/${imageName}`, import.meta.url).href
+  console.log(imgUrl)
+  return imgUrl
+
+}
 </script>
 
 <template>
   <div class="w-full h-full flex justify-center my-6">
     <div class="bg-gray-300 dark:bg-slate-700 p-4 rounded-lg h-full  w-[80%]" >
-        <img :src="'/src/assets/'+movie.image" :alt=" movie.name" class="w-full h-[85%] rounded-lg shadow-lg">
+        <img  :src="getImgURL(movie.image)" :alt=" movie.name" class="w-full h-[85%] rounded-lg shadow-lg">
         <p>{{movie.name}}</p>
   </div>
   </div>
